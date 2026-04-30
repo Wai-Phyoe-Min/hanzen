bdb0b3e99431f2a7a785ad415cc279e52a13bd71
Wai Phyoe Min <waiphyoeminmdc2592002@gmail.com>
2026-04-30 11:58:34 +0630

    Add placeholder initial migration

---
+++ restaurant/migrations/0001_initial.py
@@ -0,0 +1,11 @@
+# This is a placeholder migration - your database already has these tables
+from django.db import migrations
+
+class Migration(migrations.Migration):
+    initial = True
+    dependencies = []
+    
+    operations = [
+        # All tables already exist in your local database
+        # This file exists so Render can apply subsequent migrations
+    ]
